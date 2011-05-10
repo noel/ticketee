a470ae7ad2d8e742357bd3becbff460a4112e28b
Noel Gomez <noel_gomez@yahoo.com>
2011-05-09 21:59:14 -0700

    changed user factory and have_css in application_steps

--- factories/user_factory.rb
+++ factories/user_factory.rb
@@ -1,5 +1,5 @@
 Factory.define :user do |user| 
-  user.email { "user#{rand(10000)}@ticketee.com" } 
+  user.sequence(:email) { |n| "user#{n}@ticketee.com" } 
   user.password "password" 
   user.password_confirmation "password" 
 end
--- features/step_definitions/application_steps.rb
+++ features/step_definitions/application_steps.rb
@@ -1,13 +1,17 @@
 Then /^I should see the "([^\"]*)" link$/ do |text| 
-  css("a").detect { |a| a.text == text }.should_not(be_nil, 
-    "Expected to see the #{text.inspect} link, but did not.") 
+  # css("a").detect { |a| a.text == text }.should_not(be_nil, 
+  page.should( have_css("a", :text => text ),
+   "Expected to see the #{text.inspect} link, but did not.") 
 end 
 
 Then /^I should not see the "([^\"]*)" link$/ do |text| 
-  css("a").detect { |a| a.text == text }.should(be_nil, 
+  page.should_not( have_css("a", :text => text ),
     "Expected to not see the #{text.inspect} link, but did.") 
+  
+  # css("a").detect { |a| a.text == text }.should(be_nil, 
+    # "Expected to not see the #{text.inspect} link, but did.") 
 end 
 
-def css(selector) 
-  Nokogiri::HTML(body).css(selector) 
-end 
+# def css(selector) 
+#   Nokogiri::HTML(body).css(selector) 
+# end 
